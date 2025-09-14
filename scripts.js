301c9de340962ba2bfc85a14881b57c629a91130
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2025-09-15 09:06:04 +1000

    New gui photos

--- scripts.js
+++ scripts.js
@@ -2,23 +2,33 @@
 const images = [
     {
         src: "assets/gui1.png",
-        title: "Waveguide Filter E Field",
-        alt: "Waveguide Filter E Field"
+        title: "Series fed Patch Array 3D Farfield",
+        alt: "Series fed Patch Array 3D Farfield"
     },
     {
         src: "assets/gui2.png", 
-        title: "Waveguide Filter S-Parameters",
-        alt: "Waveguide Filter S-Parameters"
+        title: "Waveguide Filter E-Field",
+        alt: "Waveguide Filter E-Field"
     },
     {
         src: "assets/gui3.png",
-        title: "Pyramidal Horn Antenna E Field", 
-        alt: "Pyramidal Horn Antenna E Field"
+        title: "Waveguide Filter S-Parameters", 
+        alt: "Waveguide Filter S-Parameters"
+    },
+    {
+        src: "assets/gui4.png",
+        title: "Microstrip Stepped Impedance Filter",
+        alt: "Microstrip Stepped Impedance Filter"
     },
     {
         src: "assets/gui5.png",
-        title: "Pyramidal Horn Antenna Radiation Pattern",
-        alt: "Pyramidal Horn Antenna Radiation Pattern"
+        title: "Microstrip Stackup Editor",
+        alt: "Microstrip Stackup Editor"
+    },
+    {
+        src: "assets/gui6.png",
+        title: "Ridged Waveguide Horn Antenna Excitation",
+        alt: "Ridged Waveguide Horn Antenna Excitation"
     }
 ];
 
